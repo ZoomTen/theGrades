8ced822a7f1f91a39a09f5107d36dec8c7fbe1cb
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-06-23 00:42:04 +0700

    move license statement

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,26 @@
-#define APP_VERSION "0.01"
+/****************************************
+ *
+ *   theGrades - Application to log grades with GPA calculation
+ *   Copyright (C) 2020 Zumi Daxuya
+ *
+ *   Based on the24 (C) 2020 Victor Tran
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * *************************************/
+
+#define APP_VERSION "1.0"
 
 // UI includes
 #include "mainwindow.h"
