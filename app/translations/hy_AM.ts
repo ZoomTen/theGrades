a649bc689e3ab194407730508cf9d5beb675b08a
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-06-23 18:09:44 +0700

    translation spam

---
+++ app/translations/hy_AM.ts
@@ -0,0 +1,426 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="am_ET" sourcelanguage="en_US">
+<context>
+    <name>AddComponent</name>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="17"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="26"/>
+        <location filename="../grades/edit/addcomponent.cpp" line="30"/>
+        <source>Add Component</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="33"/>
+        <source>Component name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="43"/>
+        <source>Weight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="50"/>
+        <source>%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="63"/>
+        <source>Numeric Grade</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.ui" line="90"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.cpp" line="33"/>
+        <source>Edit Component</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.cpp" line="48"/>
+        <source>Name undefined!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.cpp" line="49"/>
+        <source>Please enter the component name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.cpp" line="52"/>
+        <source>Weight empty!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/addcomponent.cpp" line="53"/>
+        <source>Grade weight can&apos;t be 0.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Averages</name>
+    <message>
+        <location filename="../averages/averages.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../averages/averages.ui" line="20"/>
+        <source>Averages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../averages/averages.ui" line="36"/>
+        <source>Your calculated GPA is:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../averages/averages.ui" line="56"/>
+        <source>4.00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../averages/averages.cpp" line="29"/>
+        <source>No grades are set. Please set some in the Grades tab. If your institution uses qualitative grades, please set it also by clicking on the icon menu -&gt; Options.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../averages/averages.cpp" line="31"/>
+        <source>Your calculated GPA is</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CalcOptions</name>
+    <message>
+        <location filename="../options/calctab.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/calctab.ui" line="87"/>
+        <source>Options in this tab are automatically saved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/calctab.ui" line="100"/>
+        <source>Use qualitative (letter) grades instead of quantitative (numeric) grades</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditOrNew</name>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="20"/>
+        <source>Add / Edit Class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="72"/>
+        <source>Class Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="100"/>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="120"/>
+        <source>Grade List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="128"/>
+        <source>Component</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="133"/>
+        <source>Weight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="138"/>
+        <source>Grade</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="154"/>
+        <source>Add Component</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="177"/>
+        <location filename="../grades/edit/edit.cpp" line="32"/>
+        <source>Add Class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="190"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.ui" line="195"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.cpp" line="33"/>
+        <source>New Class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.cpp" line="36"/>
+        <location filename="../grades/edit/edit.cpp" line="37"/>
+        <source>Edit Class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.cpp" line="76"/>
+        <source>Class name empty!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.cpp" line="77"/>
+        <source>Please enter your class name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.cpp" line="80"/>
+        <source>Grade list empty!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/edit/edit.cpp" line="81"/>
+        <source>Please add some grade components.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GradeWidget</name>
+    <message>
+        <location filename="../grades/widget/gradewidget.ui" line="73"/>
+        <source>A+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/widget/gradewidget.ui" line="101"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/widget/gradewidget.ui" line="110"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/widget/gradewidget.cpp" line="36"/>
+        <source>%1 credit(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/widget/gradewidget.cpp" line="47"/>
+        <source>Class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/widget/gradewidget.cpp" line="66"/>
+        <source>Remove class?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/widget/gradewidget.cpp" line="67"/>
+        <source>Do you want to remove this class?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Grades</name>
+    <message>
+        <location filename="../grades/grades.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/grades.ui" line="20"/>
+        <source>Grades</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/grades.ui" line="54"/>
+        <source>Add Class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/grades.cpp" line="110"/>
+        <source>Removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../grades/grades.cpp" line="111"/>
+        <source>Successfully removed class from list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="51"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="14"/>
+        <source>thegrades</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="81"/>
+        <source>Averages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="97"/>
+        <source>Grades</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="154"/>
+        <source>File Bug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="163"/>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="172"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="181"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="184"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="193"/>
+        <location filename="../mainWindow.ui" line="196"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Options</name>
+    <message>
+        <location filename="../options/optionswindow.ui" line="17"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/optionswindow.ui" line="35"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/optionswindow.ui" line="88"/>
+        <source>Calculation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/optionswindow.ui" line="96"/>
+        <source>Qualitative Grades</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../main.cpp" line="62"/>
+        <source>Grade logger with GPA calculation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QualOptions</name>
+    <message>
+        <location filename="../options/qualtab.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualtab.ui" line="42"/>
+        <source>To set qualitative grades, check the &quot;qualitative grades&quot; options in the Calculation tab.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualtab.ui" line="62"/>
+        <source>Here you can set the definitions for the letter grades. These will be automatically saved once you close this window.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualtab.ui" line="72"/>
+        <source>Add Letter Grade Definition</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QualWidget</name>
+    <message>
+        <location filename="../options/qualwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualwidget.ui" line="51"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualwidget.ui" line="71"/>
+        <source>Grade:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualwidget.ui" line="100"/>
+        <source>Lower Limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualwidget.ui" line="123"/>
+        <source>A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualwidget.ui" line="168"/>
+        <source>Numeric Worth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../options/qualwidget.ui" line="181"/>
+        <source>Upper Limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
